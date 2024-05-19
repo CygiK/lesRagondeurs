81993674b2d191e90727855148ba9cb004f7fcb8
CygiK <c.hdespointes@gmail.com>
2024-05-19 15:33:08 +0200

    fix: vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/lesragondeurs/",
   plugins: [
     remix({
+      basename: '/lesragondeurs/',
       ssr: false,
     }),
     tsconfigPaths(),
